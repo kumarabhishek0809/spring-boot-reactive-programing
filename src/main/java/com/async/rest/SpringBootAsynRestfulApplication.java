1b6a0e66d1f5f434c0729ecf045a5cccb546d64b
Kumar.Abhishek <kumar.abhishek@emaratech.ae>
2018-08-06 12:08:55 +0400

    Async

---
+++ src/main/java/com/async/rest/SpringBootAsynRestfulApplication.java
@@ -0,0 +1,12 @@
+package com.async.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAsynRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAsynRestfulApplication.class, args);
+	}
+}
