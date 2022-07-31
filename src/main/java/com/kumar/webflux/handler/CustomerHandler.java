4333ea4a105bc61f06d4181acf03496c70b665f0
Kumar Abhishek <mail2kumar.2021@gmail.com>
2022-07-31 13:47:44 +0400

    updating new package
    updating new package

---
+++ src/main/java/com/kumar/webflux/handler/CustomerHandler.java
@@ -0,0 +1,43 @@
+package com.kumar.webflux.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.kumar.webflux.dao.CustomerDao;
+import com.kumar.webflux.dto.Customer;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CustomerHandler {
+
+    @Autowired
+    private CustomerDao dao;
+
+
+    public Mono<ServerResponse> loadCustomers(ServerRequest request){
+        Flux<Customer> customerList = dao.getCustomerList();
+        return ServerResponse.ok().body(customerList,Customer.class);
+    }
+
+
+    public Mono<ServerResponse> findCustomer(ServerRequest request){
+      int customerId= Integer.valueOf( request.pathVariable("input"));
+       // dao.getCustomerList().filter(c->c.getId()==customerId).take(1).single();
+        Mono<Customer> customerMono = dao.getCustomerList().filter(c -> c.getId() == customerId).next();
+        return ServerResponse.ok().body(customerMono,Customer.class);
+    }
+
+
+    public Mono<ServerResponse> saveCustomer(ServerRequest request){
+        Mono<Customer> customerMono = request.bodyToMono(Customer.class);
+        Mono<String> saveResponse = customerMono.map(dto -> dto.getId() + ":" + dto.getName());
+        return ServerResponse.ok().body(saveResponse,String.class);
+    }
+
+
+
+}
---
+++ src/main/java/com/kumar/webflux/handler/CustomerStreamHandler.java
@@ -0,0 +1,28 @@
+package com.kumar.webflux.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.kumar.webflux.dao.CustomerDao;
+import com.kumar.webflux.dto.Customer;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CustomerStreamHandler {
+
+    @Autowired
+    private CustomerDao dao;
+
+
+    public Mono<ServerResponse> getCustomers(ServerRequest request) {
+        Flux<Customer> customersStream = dao.getCustomersStream();
+        return ServerResponse.ok().
+                contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(customersStream, Customer.class);
+    }
+}
